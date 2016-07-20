652b333170ca31930b4387e9fac74e04d4f9db9f
Adrian Chadd <adrian@freebsd.org>
2016-07-19 18:14:36 -0700

    break out the evm code into a separate file.

--- src/evmlog/main.c
+++ src/evmlog/main.c
@@ -24,6 +24,8 @@
 
 #include "libradarpkt/chan.h"
 
+#include "evm.h"
+
 /* from _ieee80211.h */
 #define      IEEE80211_CHAN_HT40U    0x00020000 /* HT 40 channel w/ ext above */
 #define      IEEE80211_CHAN_HT40D    0x00040000 /* HT 40 channel w/ ext below */
@@ -51,123 +53,6 @@ pkt_compile(pcap_t *p, struct bpf_program *fp)
 	return 1;
 }
 #endif
-
-/*
- * Accessor macros to go dig through the DWORD for the relevant
- * EVM bytes.
- */
-#define	MS(_v, _f)		( ((_v) & (_f)) >> _f ## _S )
-
-#define	EVM_0		0x000000ff
-#define	EVM_0_S		0
-#define	EVM_1		0x0000ff00
-#define	EVM_1_S		8
-#define	EVM_2		0x00ff0000
-#define	EVM_2_S		16
-#define	EVM_3		0xff000000
-#define	EVM_3_S		24
-
-/*
- * The EVM pilot information representation, post-processed.
- */
-#define	NUM_EVM_PILOTS		6
-#define	NUM_EVM_STREAMS		3
-struct evm {
-	int8_t	evm_pilots[NUM_EVM_STREAMS][NUM_EVM_PILOTS];
-	int num_pilots;
-	int num_streams;
-};
-
-/* Print the EVM information */
-static void
-print_evm(struct evm *e)
-{
-	int s, p;
-
-	for (s = 0; s < e->num_streams; s++) {
-		printf(" evm_stream_%d=[", s + 1);
-		for (p = 0; p < NUM_EVM_PILOTS; p++) {
-			printf(" %d", (int) (e->evm_pilots[s][p]));
-		}
-		printf(" ]");
-	}
-}
-
-/*
- * Populate the given EVM information.
- *
- * The EVM pilot offsets depend upon whether the rates are
- * 1, 2 or 3 stream, as well as HT20 or HT40.
- */
-static void
-populate_evm(struct evm *e, uint32_t evm[4], uint8_t rx_hwrate, int rx_isht40)
-{
-	/* Initialise everything to 0x80 - invalid */
-	memset(e->evm_pilots, 0x80, sizeof(e->evm_pilots));
-
-	/* HT20 pilots - always 4 */
-	e->num_pilots = 4;
-	if (rx_isht40)
-		e->num_pilots += 2;	/* HT40 - 6 pilots */
-
-	/* XXX assume it's MCS */
-	if (rx_hwrate < 0x88) {		/* 1 stream */
-		e->num_streams = 1;
-		e->evm_pilots[0][0] = MS(evm[0], EVM_0);
-		e->evm_pilots[0][1] = MS(evm[0], EVM_1);
-		e->evm_pilots[0][2] = MS(evm[0], EVM_2);
-		e->evm_pilots[0][3] =
-		    MS(evm[0], EVM_3);
-		if (rx_isht40) {
-			e->evm_pilots[0][4] = MS(evm[1], EVM_0),
-			e->evm_pilots[0][5] = MS(evm[1], EVM_1);
-		}
-	} else if (rx_hwrate < 0x90) {	/* 2 stream */
-		e->num_streams = 2;
-		e->evm_pilots[0][0] = MS(evm[0], EVM_0);
-		e->evm_pilots[0][1] = MS(evm[0], EVM_2);
-		e->evm_pilots[0][2] = MS(evm[1], EVM_0);
-		e->evm_pilots[0][3] = MS(evm[1], EVM_2);
-		if (rx_isht40) {
-			e->evm_pilots[0][4] = MS(evm[2], EVM_0),
-			e->evm_pilots[0][5] = MS(evm[2], EVM_2);
-		}
-		e->evm_pilots[1][0] = MS(evm[0], EVM_1);
-		e->evm_pilots[1][1] = MS(evm[0], EVM_3);
-		e->evm_pilots[1][2] = MS(evm[1], EVM_1);
-		e->evm_pilots[1][3] = MS(evm[1], EVM_3);
-		if (rx_isht40) {
-			e->evm_pilots[1][4] = MS(evm[2], EVM_1);
-			e->evm_pilots[1][5] = MS(evm[2], EVM_3);
-		}
-	} else {			/* 3 stream */
-		e->num_streams = 3;
-		e->evm_pilots[0][0] = MS(evm[0], EVM_0);
-		e->evm_pilots[0][1] = MS(evm[0], EVM_3);
-		e->evm_pilots[0][2] = MS(evm[1], EVM_2);
-		e->evm_pilots[0][3] = MS(evm[2], EVM_1);
-		if (rx_isht40) {
-			e->evm_pilots[0][4] = MS(evm[3], EVM_0);
-			e->evm_pilots[0][5] = MS(evm[3], EVM_3);
-		}
-		e->evm_pilots[1][0] = MS(evm[0], EVM_1);
-		e->evm_pilots[1][1] = MS(evm[1], EVM_0);
-		e->evm_pilots[1][2] = MS(evm[1], EVM_3);
-		e->evm_pilots[1][3] = MS(evm[2], EVM_2);
-		if (rx_isht40) {
-			e->evm_pilots[1][4] = MS(evm[3], EVM_1);
-			e->evm_pilots[1][5] = MS(evm[4], EVM_0);
-		}
-		e->evm_pilots[2][0] = MS(evm[0], EVM_2);
-		e->evm_pilots[2][1] = MS(evm[1], EVM_1);
-		e->evm_pilots[2][2] = MS(evm[2], EVM_0);
-		e->evm_pilots[2][3] = MS(evm[2], EVM_3);
-		if (rx_isht40) {
-			e->evm_pilots[2][4] = MS(evm[3], EVM_2);
-			e->evm_pilots[2][5] = MS(evm[4], EVM_1);
-		}
-	}
-}
 
 void
 pkt_handle(int chip, const char *pkt, int len)
